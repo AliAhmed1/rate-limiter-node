9e95d1bcc876b5d162fd364755d5dba47f5131d5
aliahmed1 <saliahmed11111@gmail.com>
2024-11-01 01:14:51 +0500

    node.js solution

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,41 @@
 class RateLimiterMiddleware {
   constructor(options) {
     this.rateLimitStore = new Map();
+    this.maxRequests = options.maxRequests;
+    this.timeWindow = options.timeWindow;
     this.options = options;
   }
 
   middleware() {
     return (req, res, next) => {
+      const clientIP = req.ip || req.connection.remoteAddress
+      const currentTime = Date.now();
+
+      let requestData = this.rateLimitStore.get(clientIP) || {
+        requestCount: 0,
+        firstRequestTime: currentTime
+      };
+
+      if (currentTime - requestData.firstRequestTime >= this.timeWindow) {
+        requestData.requestCount = 1;
+        requestData.firstRequestTime = currentTime;
+      } else {
+        requestData.requestCount += 1;
+      }
+
+      if (requestData.requestCount > this.maxRequests) {
+        res.set('X-RateLimit-Limit', this.maxRequests);
+        res.set('X-RateLimit-Remaining', 0);
+        res.set('X-RateLimit-Reset', requestData.firstRequestTime + +(this.timeWindow));
+
+        return res.status(429).json({ message: "You have exceeded the rate limit. Please try again later." });
+      }
+
+      this.rateLimitStore.set(clientIP, requestData);
+      res.set('X-RateLimit-Limit', this.maxRequests);
+      res.set('X-RateLimit-Remaining', this.maxRequests - requestData.requestCount);
+      res.set('X-RateLimit-Reset', requestData.firstRequestTime + +(this.timeWindow));
+
       next();
     };
   }
